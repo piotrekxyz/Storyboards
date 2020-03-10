44c1a2925bfd753e0bed6dfa7f61e8fd04eba125
piotrekxyz <piotrekxyz@tlen.pl>
2020-03-10 09:29:32 +0100

    Animacje

---
+++ Obrazki/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Obrazki
+{
+	public partial class MainWindow : Window
+	{
+		public bool Rob { get; set; } = true;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
